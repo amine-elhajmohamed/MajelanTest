825e69a76391813dbe7f42c4d987b02b55df3829
Mohamed Amine Hadj Mohamed <mahm.ze@gmail.com>
2019-07-23 22:23:26 +0200

    Initial Commit

---
+++ MajelanTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MajelanTest
+//
+//  Created by Amine Hadj Mohamed on 23/7/19.
+//  Copyright © 2019 Amine Haj Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
